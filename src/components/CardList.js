9ee0ed40dc98b69be9858ffa430d282f8a53b50c
NelsonCollins <nelsoncollins90@gmail.com>
2020-09-18 13:23:44 +0100

    Minor Commit

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Card from './Card'
 const CardList = ({robot})=>{
 	// if (true){
-	// 	throw new Error('Nooooo!');
+	// 	throw new Error('Nooooo Nelson!');
 	// }
 	return(
 		 <div>
